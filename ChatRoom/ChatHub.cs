ae022cb263343e5e29e301a7211b7c7ff27f5eb3
sebascarag <sebastiansca10@gmail.com>
2022-11-17 23:53:07 -0500

    Add project files.

---
+++ ChatRoom/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatRoom
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string room, string user, string message)
+        {
+            await Clients.Group(room).SendAsync("ReceiveMessage", user, message); 
+        }
+
+        public async Task AddToGroup(string room, string user)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+            await Clients.Group(room).SendAsync("ShowWho", $"{DateTime.Now:dd/MMM HH:mm:ss} - Se conectó {user} : {Context.ConnectionId}");
+
+        }
+    }
+}
---
+++ ChatRoom/Controllers/ChatController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatRoom.Controllers
+{
+    public class ChatController : Controller
+    {
+        public static Dictionary<int, string> Rooms = new Dictionary<int, string>()
+        {
+            {1, "Cerveza" },
+            {2, "Programacion" },
+            {3, "Chisme" }
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Room(int roomId)
+        {
+            return View("Room", roomId);
+        }
+    }
+}
